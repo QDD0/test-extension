c0caf83763b7323931080f7e8d1f041c92f134cf
Den <dend51108@gmail.com>
2026-03-02 20:48:35 +0300

    Initial commit

---
+++ src/main/java/org/example/testextension/TestExtensionApplication.java
@@ -0,0 +1,13 @@
+package org.example.testextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestExtensionApplication.class, args);
+    }
+
+}
